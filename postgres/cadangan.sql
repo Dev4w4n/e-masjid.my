2f2fc7b9421c4c84f232c4ce29debfda6e499ed9
izandotnet <rohaizanr@gmail.com>
2023-12-22 01:46:24 +0800

    Fix cadangan DB

--- postgres/cadangan.sql
+++ postgres/cadangan.sql
@@ -9,7 +9,7 @@ insert into cadangan_types(name) values ('Lain-lain');
 CREATE TABLE cadangan (
     id serial PRIMARY KEY,
     cadangan_types_id INTEGER REFERENCES cadangan_types(id),
-    cadangan_text VARCHAR(1024) IS NOT NULL,
+    cadangan_text VARCHAR(1024) NOT NULL,
     tindakan_text VARCHAR(1024),
     is_open BOOLEAN DEFAULT false,
     score smallint,
