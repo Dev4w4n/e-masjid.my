0cfbaaaf2236852abed4bf22438789fde6756e57
Eros Anwar <a_oih81@hotmail.com>
2024-01-19 17:01:49 +0800

    Finish Delete. Simplify web error message.

--- api/tetapan-api-go/controller/tetapan_controller.go
+++ api/tetapan-api-go/controller/tetapan_controller.go
@@ -1,11 +1,11 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/Dev4w4n/e-masjid.my/api/tetapan-api-go/model"
 	"github.com/Dev4w4n/e-masjid.my/api/tetapan-api-go/repository"
+	"github.com/Dev4w4n/e-masjid.my/api/tetapan-api-go/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 )
@@ -25,6 +25,7 @@ func NewTetapanController(engine *gin.Engine, repo repository.TetapanRepository)
 	controller.engine.GET("/tetapan/:kunci", controller.FindByKunci)
 	controller.engine.POST("/tetapan", controller.Save)
 	controller.engine.POST("/tetapan/senarai", controller.SaveAll)
+	controller.engine.DELETE("/tetapan/:kunci", controller.Delete)
 
 	return controller
 }
@@ -32,14 +33,8 @@ func NewTetapanController(engine *gin.Engine, repo repository.TetapanRepository)
 func (controller *TetapanController) FindAll(ctx *gin.Context) {
 	log.Info().Msg("find all tetapan")
 
-	var errorMessage string
 	result, err := controller.tetapanRepository.FindAll()
-
-	if err != nil {
-		errorMessage = fmt.Sprintf("failed to retrieve tetapan list: %v", err)
-		log.Error().Msg(errorMessage)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errorMessage})
-	}
+	utils.WebError(ctx, err, "failed to retrieve tetapan list")
 
 	ctx.Header("Content-Type", "application/json")
 	ctx.JSON(http.StatusOK, result)
@@ -48,15 +43,9 @@ func (controller *TetapanController) FindAll(ctx *gin.Context) {
 func (controller *TetapanController) FindByKunci(ctx *gin.Context) {
 	log.Info().Msg("findbykunci tetapan")
 
-	var errorMessage string
 	kunci := ctx.Param("kunci")
 	result, err := controller.tetapanRepository.FindByKunci(kunci)
-
-	if err != nil {
-		errorMessage = fmt.Sprintf("failed to retrieve tetapan: %v", err)
-		log.Error().Msg(errorMessage)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errorMessage})
-	}
+	utils.WebError(ctx, err, "failed to retrieve tetapan")
 
 	ctx.Header("Content-Type", "application/json")
 	ctx.JSON(http.StatusOK, result)
@@ -65,23 +54,12 @@ func (controller *TetapanController) FindByKunci(ctx *gin.Context) {
 func (controller *TetapanController) Save(ctx *gin.Context) {
 	log.Info().Msg("save tetapan")
 
-	var errorMessage string
 	saveTetapan := model.Tetapan{}
 	err := ctx.ShouldBindJSON(&saveTetapan)
-
-	if err != nil {
-		errorMessage = fmt.Sprintf("failed to bind JSON: %v", err)
-		log.Error().Msg(errorMessage)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errorMessage})
-	}
+	utils.WebError(ctx, err, "failed to bind JSON")
 
 	err = controller.tetapanRepository.Save(saveTetapan)
-
-	if err != nil {
-		errorMessage = fmt.Sprintf("failed to save tetapan: %v", err)
-		log.Error().Msg(errorMessage)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errorMessage})
-	}
+	utils.WebError(ctx, err, "failed to save tetapan")
 
 	ctx.Header("Content-Type", "application/json")
 	ctx.Status(http.StatusOK)
@@ -90,23 +68,23 @@ func (controller *TetapanController) Save(ctx *gin.Context) {
 func (controller *TetapanController) SaveAll(ctx *gin.Context) {
 	log.Info().Msg("save all tetapan")
 
-	var errorMessage string
 	saveTetapanList := []model.Tetapan{}
 	err := ctx.ShouldBindJSON(&saveTetapanList)
-
-	if err != nil {
-		errorMessage = fmt.Sprintf("failed to bind JSON: %v", err)
-		log.Error().Msg(errorMessage)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errorMessage})
-	}
+	utils.WebError(ctx, err, "failed to bind JSON")
 
 	err = controller.tetapanRepository.SaveAll(saveTetapanList)
+	utils.WebError(ctx, err, "failed to save tetapan")
 
-	if err != nil {
-		errorMessage = fmt.Sprintf("failed to save tetapan: %v", err)
-		log.Error().Msg(errorMessage)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errorMessage})
-	}
+	ctx.Header("Content-Type", "application/json")
+	ctx.Status(http.StatusOK)
+}
+
+func (controller *TetapanController) Delete(ctx *gin.Context) {
+	log.Info().Msg("delete tetapan")
+
+	kunci := ctx.Param("kunci")
+	err := controller.tetapanRepository.Delete(kunci)
+	utils.WebError(ctx, err, "failed to delete tetapan")
 
 	ctx.Header("Content-Type", "application/json")
 	ctx.Status(http.StatusOK)
--- api/tetapan-api-go/repository/tetapan_repository.go
+++ api/tetapan-api-go/repository/tetapan_repository.go
@@ -10,6 +10,7 @@ type TetapanRepository interface {
 	FindByKunci(kunci string) (model.Tetapan, error)
 	Save(tetapan model.Tetapan) error
 	SaveAll(tetapanList []model.Tetapan) error
+	Delete(kunci string) error
 }
 
 type TetapanRepositoryImpl struct {
@@ -56,6 +57,17 @@ func (repo *TetapanRepositoryImpl) Save(tetapan model.Tetapan) error {
 
 func (repo *TetapanRepositoryImpl) SaveAll(tetapanList []model.Tetapan) error {
 	result := repo.Db.Save(tetapanList)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (repo *TetapanRepositoryImpl) Delete(kunci string) error {
+	var tetapan model.Tetapan
+	result := repo.Db.Where("kunci = ?", kunci).Delete(&tetapan)
 
 	if result.Error != nil {
 		return result.Error
