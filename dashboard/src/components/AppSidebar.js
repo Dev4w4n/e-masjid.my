b71efb71eefcbf6a63436b3c88b847052745b273
adamjanin <adamjanin23@gmail.com>
2024-04-30 07:05:29 +0800

    update package and disable warning message

--- dashboard/src/components/AppSidebar.js
+++ dashboard/src/components/AppSidebar.js
@@ -9,8 +9,8 @@ import { AppSidebarNav } from './AppSidebarNav'
 import { logo } from 'src/assets/brand/logo'
 import { sygnet } from 'src/assets/brand/sygnet'
 
-import SimpleBar from 'simplebar-react'
-import 'simplebar/dist/simplebar.min.css'
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 
 // sidebar nav config
 import navigation from '../_nav'
