64c5effdae46ac65a7335fd1304ef4ddab3460a1
Hisyam Nasir <hisyam@terato.com>
2024-05-01 17:03:10 +0800

    fix: rename prettier and  update include

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
     resolve: {
@@ -17,6 +17,6 @@ export default defineConfig(({ mode }) => {
     },
     define: {
       'process.env': env,
-    }
+    },
   }
 })
