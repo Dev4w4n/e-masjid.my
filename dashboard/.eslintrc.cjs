ab7cf1852818b3d7840fa7276dd1b1121d77fe92
Hisyam Nasir <hisyam@terato.com>
2024-05-03 18:41:09 +0800

    update eslint

--- dashboard/.eslintrc.cjs
+++ dashboard/.eslintrc.cjs
@@ -1,28 +1,37 @@
-module.exports = {
+/** @type {import("eslint").Linter.Config} */
+const config = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    // 'plugin:react-hooks/recommended',
+    // 'plugin:react/recommended',
+    // 'plugin:react/jsx-runtime',
     'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'build', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
     tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-  overrides: [
-    {
-      files: ['*.js', '*.jsx'],
-      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+  // overrides: [
+  //   {
+  //     files: ['*.js', '*.jsx'],
+  //     extends: ['plugin:@typescript-eslint/disable-type-checked'],
+  //   },
+  // ],
+  settings: {
+    react: {
+      version: 'detect',
     },
-  ],
+  },
 }
+
+module.exports = config
