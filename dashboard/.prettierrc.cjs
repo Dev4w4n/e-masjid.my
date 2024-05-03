0f53a1f08a93a183d4a00013b82cbec6dc2051f2
Hisyam Nasir <hisyam@terato.com>
2024-05-03 19:07:13 +0800

    add sort imports plugins

--- dashboard/.prettierrc.cjs
+++ dashboard/.prettierrc.cjs
@@ -4,4 +4,8 @@ module.exports = {
   singleQuote: true,
   printWidth: 100,
   tabWidth: 2,
+  plugins: [require('@ianvs/prettier-plugin-sort-imports')],
+  importOrder: ['^react$', '', '<THIRD_PARTY_MODULES>', '', '^~/', '^@/', '^[.][.]/', '^[.]/'],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '4.4.0',
 }
