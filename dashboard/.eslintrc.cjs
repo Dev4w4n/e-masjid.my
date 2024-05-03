58e0d6decd5356bc9a341e01be2ad7b4f0524139
Hisyam Nasir <hisyam@terato.com>
2024-05-03 18:50:33 +0800

    install eslint isaacscript plugin and add new eslint rules

--- dashboard/.eslintrc.cjs
+++ dashboard/.eslintrc.cjs
@@ -17,9 +17,32 @@ const config = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-  plugins: ['react-refresh', 'import'],
+  plugins: ['isaacscript', 'react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+    // These off/not-configured-the-way-we-want lint rules we like & opt into
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+    ],
+    'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+
+    // For educational purposes we format our comments/jsdoc nicely
+    'isaacscript/complete-sentences-jsdoc': 'warn',
+    'isaacscript/format-jsdoc-comments': 'warn',
+
+    // These lint rules don't make sense for us but are enabled in the preset configs
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+
+    // This rule doesn't seem to be working properly
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
   },
   overrides: [
     {
