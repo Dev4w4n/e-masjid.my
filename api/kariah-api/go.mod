6a5c5e86dfeddde54d2129d2832ca123b0846d97
rohaizanr <rohaizanr@gmail.com>
2024-10-13 20:54:37 +0800

    Kariah API Project & Models

---
+++ api/kariah-api/go.mod
@@ -0,0 +1,68 @@
+module github.com/Dev4w4n/e-masjid.my/api/kariah-api
+
+go 1.22.1
+
+require (
+	github.com/Dev4w4n/e-masjid.my/api/core/env v0.0.0-20240917155547-fc3a0b4b6299
+	github.com/Dev4w4n/e-masjid.my/api/core/security v0.0.0-20240917155547-fc3a0b4b6299
+	github.com/Dev4w4n/e-masjid.my/api/tabung-api v0.0.0-20240917155547-fc3a0b4b6299
+	github.com/Dev4w4n/e-masjid.my/saas v0.0.0-20240917155547-fc3a0b4b6299
+	github.com/gin-contrib/cors v1.7.2
+)
+
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/go-resty/resty/v2 v2.13.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggo/swag v1.16.3 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	gorm.io/driver/postgres v1.5.9 // indirect
+	gorm.io/gorm v1.25.10 // indirect
+)
+
+require (
+	github.com/bytedance/sonic v1.11.7 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.10.0
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.20.0 // indirect
+	github.com/go-saas/saas v0.6.3
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/swaggo/files v1.0.1
+	github.com/swaggo/gin-swagger v1.6.0
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	golang.org/x/arch v0.8.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- saas/005_kariah.sql
+++ saas/005_kariah.sql
@@ -1,25 +1,43 @@
--- CREATE TABLE kariah_dependents(
---     id SERIAL PRIMARY KEY,
---     person_id INTEGER REFERENCES person(id),
---     hubungan_type_id INTEGER REFERENCES tetapan_types(id)
--- );
+CREATE TABLE kariah_dependents(
+    id SERIAL PRIMARY KEY,
+    person_id INTEGER REFERENCES person(id),
+    hubungan_type_id INTEGER REFERENCES tetapan_types(id)
+);
 
--- CREATE TABLE kariah_members(
---     id SERIAL PRIMARY KEY,
---     person_id INTEGER REFERENCES person(id),
---     bangsa INTEGER REFERENCES tetapan_types(id),
---     pekerjaan INTEGER REFERENCES tetapan_types(id),
---     warganegara INTEGER REFERENCES tetapan_types(id),
---     status_perkahwinan INTEGER REFERENCES tetapan_types(id),
---     is_khairat_kematian BOOLEAN DEFAULT(FALSE),
---     is_oku BOOLEAN DEFAULT(FALSE),
---     file_id_bil_utiliti INTEGER REFERENCES uploaded_files_cdn(id),
---     file_id_gambar INTEGER REFERENCES uploaded_files_cdn(id)
--- );
+CREATE TABLE kariah_members(
+    id SERIAL PRIMARY KEY,
+    person_id INTEGER REFERENCES person(id),
+    bangsa_id INTEGER REFERENCES tetapan_types(id),
+    pekerjaan_id INTEGER REFERENCES tetapan_types(id),
+    warganegara_id INTEGER REFERENCES tetapan_types(id),
+    status_perkahwinan_id INTEGER REFERENCES tetapan_types(id),
+    is_khairat_kematian BOOLEAN DEFAULT(FALSE),
+    is_oku BOOLEAN DEFAULT(FALSE),
+    file_id_bil_utiliti INTEGER REFERENCES uploaded_files_cdn(id),
+    file_id_gambar INTEGER REFERENCES uploaded_files_cdn(id)
+);
 
--- CREATE TABLE kariah_members_assigned_types(
---     id SERIAL PRIMARY KEY,
---     kariah_member_id INTEGER REFERENCES kariah_members(id),
---     tetapan_types_id INTEGER REFERENCES tetapan_types(id),
---     tetapan_types_group_name VARCHAR(32)
--- );
+CREATE TABLE kariah_members_assigned_types(
+    id SERIAL PRIMARY KEY,
+    kariah_member_id INTEGER REFERENCES kariah_members(id),
+    tetapan_types_id INTEGER REFERENCES tetapan_types(id),
+    tetapan_types_group_name VARCHAR(32)
+);
+
+-- Indexes for kariah_dependents
+CREATE INDEX idx_kariah_dependents_person_id ON kariah_dependents(person_id);
+CREATE INDEX idx_kariah_dependents_hubungan_type_id ON kariah_dependents(hubungan_type_id);
+
+-- Indexes for kariah_members
+CREATE INDEX idx_kariah_members_person_id ON kariah_members(person_id);
+CREATE INDEX idx_kariah_members_bangsa_id ON kariah_members(bangsa_id);
+CREATE INDEX idx_kariah_members_pekerjaan_id ON kariah_members(pekerjaan_id);
+CREATE INDEX idx_kariah_members_warganegara_id ON kariah_members(warganegara_id);
+CREATE INDEX idx_kariah_members_status_perkahwinan_id ON kariah_members(status_perkahwinan_id);
+CREATE INDEX idx_kariah_members_file_id_bil_utiliti ON kariah_members(file_id_bil_utiliti);
+CREATE INDEX idx_kariah_members_file_id_gambar ON kariah_members(file_id_gambar);
+
+-- Indexes for kariah_members_assigned_types
+CREATE INDEX idx_kariah_members_assigned_types_member_id ON kariah_members_assigned_types(kariah_member_id);
+CREATE INDEX idx_kariah_members_assigned_types_tetapan_types_id ON kariah_members_assigned_types(tetapan_types_id);
+CREATE INDEX idx_kariah_members_assigned_types_group_name ON kariah_members_assigned_types(tetapan_types_group_name);
