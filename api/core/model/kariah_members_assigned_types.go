71f214560559613c9821fb9823b5d5f255ceabf4
rohaizanr <rohaizanr@gmail.com>
2024-10-13 22:07:14 +0800

    Init model project

--- api/core/model/kariah_members_assigned_types.go
+++ api/core/model/kariah_members_assigned_types.go
@@ -9,7 +9,7 @@ type KariahMemberAssignedType struct {
 	ID                    uint   `gorm:"column:id;primaryKey" json:"id"`
 	KariahMemberID        uint   `gorm:"column:kariah_member_id" json:"kariahMemberId"`
 	TetapanTypesID        uint   `gorm:"column:tetapan_types_id" json:"tetapanTypesId"`
-	TetapanTypesGroupName string `gorm:"column:tetapan_types_group_name" json:"tetapanTypesGroupName" size:32`
+	TetapanTypesGroupName string `gorm:"column:tetapan_types_group_name" json:"tetapanTypesGroupName"`
 	gorm.Model            `swaggerignore:"true"`
 	gorm2.MultiTenancy    `swaggerignore:"true"`
 }
