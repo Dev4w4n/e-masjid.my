f283d84e0503aff80a74a2610bc3de339845a398
rohaizanr <rohaizanr@gmail.com>
2024-10-21 01:33:03 +0800

    save my work

--- public-web/src/components/Kariah/NavItem/MaklumatDiri/Kontak.jsx
+++ public-web/src/components/Kariah/NavItem/MaklumatDiri/Kontak.jsx
@@ -57,20 +57,21 @@ export default function Kontak({ activeKey }) {
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
               <label htmlFor="nama-penuh" className="block text-sm font-medium leading-6 text-gray-900">
-                Alamat 1
+                Alamat
               </label>
               <div className="mt-2">
-                <input
+                <textarea
                   id="nama-penuh"
                   name="nama-penuh"
-                  type="text"
+                  rows="4" 
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
           </div>
-          
+
+
         </div>
 
       </div>
