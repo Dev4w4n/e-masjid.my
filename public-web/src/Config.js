59ff069975bf685a859e62c65bf8ccfbf734b54a
rohaizanr <rohaizanr@gmail.com>
2024-11-20 19:26:07 +0800

    save my work

--- public-web/src/Config.js
+++ public-web/src/Config.js
@@ -1,36 +1,46 @@
-const getSubdomain = () => {
-  const parts = window.location.hostname.split('.')
+// Helper function to extract the subdomain from the hostname
+export const getSubdomain = () => {
+  const parts = window.location.hostname.split(".");
   if (parts.length >= 3) {
-    return parts[0]
+    return parts[0];
   }
-  return null
-}
+  return null;
+};
 
-const subdomain = getSubdomain()
-const dynamicSubdomain = subdomain === 'localhost' ? 'demo' : subdomain
+// Export the dynamic subdomain
+export const dynamicSubdomain = (() => {
+  const subdomain = getSubdomain();
+  return subdomain === "localhost" ? "demo" : subdomain;
+})();
 
+// Base configuration
+const baseConfig = {
+  version: {
+    BUILD: process.env.BUILD_VERSION || "v2.0.0", // Default to v2.0.0 if not provided
+  },
+  url: {
+    CADANGAN_API_BASE_URL: "",
+    TETAPAN_API_BASE_URL: "",
+  },
+};
+
+// Production environment configuration
 const prod = {
-    version: {
-      BUILD: 'v2.0.0',
-    },
-    url: {
-      KEYCLOAK_BASE_URL: 'https://loginv2.e-masjid.my',
-      KEYCLOAK_REALM: dynamicSubdomain,
-      KEYCLOAK_CLIENT_ID: `${dynamicSubdomain}-auth`,
-      CADANGAN_API_BASE_URL: 'https://demoapi.e-masjid.my/api-public',
-      TETAPAN_API_BASE_URL: 'https://demoapi.e-masjid.my/api-public',
-    },
-  }
-  
-  const dev = {
-    version: {
-      BUILD: 'v2.0.0',
-    },
-    url: {
-      CADANGAN_API_BASE_URL: 'http://localhost:8084',
-      TETAPAN_API_BASE_URL: 'http://localhost:8086',
-    },
-  }
-  
-  export const config = process.env.REACT_APP_ENV === 'development' ? dev : prod
-  
+  ...baseConfig,
+  url: {
+    CADANGAN_API_BASE_URL: `https://${dynamicSubdomain}.${process.env.DOMAIN}/public`,
+    TETAPAN_API_BASE_URL: `https://${dynamicSubdomain}.${process.env.DOMAIN}/public`,
+  },
+};
+
+// Development environment configuration
+const dev = {
+  ...baseConfig,
+  url: {
+    CADANGAN_API_BASE_URL: "http://localhost:8084",
+    TETAPAN_API_BASE_URL: "http://localhost:8086",
+  },
+};
+
+// Export the final configuration based on the environment
+export const config = process.env.REACT_APP_ENV === "development" ? dev : prod;
